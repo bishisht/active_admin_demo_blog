fd7d68d3b125e8dd6fbf13be5896b5423d2e5da8
Bishisht Bhatta <bishisht.np.mnr@gmail.com>
2015-06-20 16:58:24 +0545

    Added Post model to Active Admin.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20150620084241) do
+ActiveRecord::Schema.define(:version => 20150620085128) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.string   "namespace"
@@ -45,5 +45,12 @@ ActiveRecord::Schema.define(:version => 20150620084241) do
 
   add_index "admin_users", ["email"], :name => "index_admin_users_on_email", :unique => true
   add_index "admin_users", ["reset_password_token"], :name => "index_admin_users_on_reset_password_token", :unique => true
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
